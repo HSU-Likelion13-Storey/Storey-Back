fd9836c3810c76528391beef77d36302a7afa807
zzuhannn <lunaaeric41@naver.com>
2025-08-07 01:00:06 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sixjeon/storey/StoreyApplication.java
@@ -0,0 +1,13 @@
+package com.sixjeon.storey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreyApplication.class, args);
+	}
+
+}
