18191c65ea0cdc5088771e02a2a7122ae3d41679
zzuhannn <lunaaeric41@naver.com>
2025-08-07 17:55:34 +0900

    feat: 아이디 중복 예외 처리

---
+++ src/main/java/com/sixjeon/storey/domain/auth/exception/AuthErrorCode.java
@@ -0,0 +1,15 @@
+package com.sixjeon.storey.domain.auth.exception;
+
+import com.sixjeon.storey.global.response.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorCode implements BaseResponseCode {
+    AUTH_DUPLICATE_LOGINID_409("AUTH_DULICATE_LOGINID_409",409,"이미 존재하는 아이디입니다.");
+
+    private final String code;
+    private final int httpStatus;
+    private final String message;
+}
