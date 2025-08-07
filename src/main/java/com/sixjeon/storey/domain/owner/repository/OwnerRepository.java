667d773563c4aa99bbeae7cfd05ce218093a5e27
zzuhannn <lunaaeric41@naver.com>
2025-08-07 19:17:56 +0900

    feat: repository 어노테이션 추가

--- src/main/java/com/sixjeon/storey/domain/owner/repository/OwnerRepository.java
+++ src/main/java/com/sixjeon/storey/domain/owner/repository/OwnerRepository.java
@@ -2,7 +2,9 @@ package com.sixjeon.storey.domain.owner.repository;
 
 import com.sixjeon.storey.domain.owner.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
     boolean existsByLoginId(String loginId);
 }
