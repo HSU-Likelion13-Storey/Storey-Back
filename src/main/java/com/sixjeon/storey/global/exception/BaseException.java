12b4d0eadc25ed3e0d9983fe32006abf419a0f23
zzuhannn <lunaaeric41@naver.com>
2025-08-07 11:30:08 +0900

    feat: global 패키지 추가
    - WebConfig : CORS 설정을 통해 다른 도메인에서의 API 접근 허용
    - StaticValue : 애플리케이션 전역에서 사용되는 상수 값들 정의
    - BaseEntity : JPA 엔티티 클래스들의 공통 속성을 정의한 기본 클래스
    - BaseException : 비즈니스 로직에서 발생하는 예외를 표현함
    - GlobalExceptionHandler : 전역에서 발생하는 예외를 처리하는 핸들러
    - response 패키지 : API 응답에 대한 공통적인 구조와 처리를 정의함

---
+++ src/main/java/com/sixjeon/storey/global/exception/BaseException.java
@@ -0,0 +1,12 @@
+package com.sixjeon.storey.global.exception;
+
+import com.sixjeon.storey.global.response.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// 비즈니스 레이어에서 예외 상황을 표현하는 커스텀 런타임 예외 클래스
+@Getter
+@AllArgsConstructor
+public class BaseException extends RuntimeException{
+    private final BaseResponseCode baseResponseCode;
+}
---
+++ src/main/java/com/sixjeon/storey/global/response/code/BaseResponseCode.java
@@ -0,0 +1,9 @@
+package com.sixjeon.storey.global.response.code;
+
+//공통 응답 코드 인터페이스
+public interface BaseResponseCode {
+    String getCode();
+    int getHttpStatus();
+    String getMessage();
+
+}
