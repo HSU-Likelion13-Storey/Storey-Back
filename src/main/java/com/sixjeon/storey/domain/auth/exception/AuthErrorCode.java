a400936e831cd96b0e187f3d9e2ba5b41a5c60d3
zzuhannn <lunaaeric41@naver.com>
2025-08-07 19:24:06 +0900

    feat: 유효하지 않는 타입 예외 처리 추가

--- src/main/java/com/sixjeon/storey/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/sixjeon/storey/domain/auth/exception/AuthErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AuthErrorCode implements BaseResponseCode {
-    AUTH_DUPLICATE_LOGINID_409("AUTH_DULICATE_LOGINID_409",409,"이미 존재하는 아이디입니다.");
+    AUTH_DUPLICATE_LOGINID_409("AUTH_DULICATE_LOGINID_409",409,"이미 존재하는 아이디입니다."),
+    ROLE_INVALID_400("ROLE_INVALID_400",400,"유효하지 않은 사용자 유형입니다.");
 
     private final String code;
     private final int httpStatus;
