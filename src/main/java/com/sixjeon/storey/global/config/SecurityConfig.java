70a6d7f1f3076efec7909117c5effe47dd4a7bf6
zzuhannn <lunaaeric41@naver.com>
2025-08-08 00:55:33 +0900

    feat: 인증 API를 위한 Spring Security 설정 추가

--- src/main/java/com/sixjeon/storey/global/config/SecurityConfig.java
+++ src/main/java/com/sixjeon/storey/global/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.sixjeon.storey.global.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +16,19 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
 
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().permitAll()
+                );
+
+
+        return http.build();
     }
 }
