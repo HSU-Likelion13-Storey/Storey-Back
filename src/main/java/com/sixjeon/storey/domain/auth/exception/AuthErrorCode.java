25a6268d5e35b6829ccf137e3cd93ebc2920d831
zzuhannn <lunaaeric41@naver.com>
2025-08-08 21:31:53 +0900

    feat: 비밀번호 불일치 예외 커스텀 추가

--- src/main/java/com/sixjeon/storey/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/sixjeon/storey/domain/auth/exception/AuthErrorCode.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 public enum AuthErrorCode implements BaseResponseCode {
     AUTH_DUPLICATE_LOGINID_409("AUTH_DULICATE_LOGINID_409",409,"이미 존재하는 아이디입니다."),
     AUTH_DUPLICATE_PHONENUMBER_409("AUTH_DUPLICATE_PHONENUMBER_409",409,"이미 존재하는 전화번호 입니다."),
+    AUTH_INVALID_PASSWORD_401("AUTH_INVALID_PASSWORD_401", 401, "비밀번호가 일치하지 않습니다."),
     ROLE_INVALID_400("ROLE_INVALID_400",400,"유효하지 않은 사용자 유형입니다.");
 
     private final String code;
