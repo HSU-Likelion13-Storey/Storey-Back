17474821565cd7bc4c7e5bcf3d8f45cb4e69cedc
zzuhannn <lunaaeric41@naver.com>
2025-08-09 17:28:07 +0900

    refactor: 인증 정보를 토큰에 담아 res 필드 변경

--- src/main/java/com/sixjeon/storey/domain/auth/web/dto/LoginRes.java
+++ src/main/java/com/sixjeon/storey/domain/auth/web/dto/LoginRes.java
@@ -1,11 +1,6 @@
 package com.sixjeon.storey.domain.auth.web.dto;
 
-import com.sixjeon.storey.domain.auth.entity.Role;
-
 public record LoginRes(
-        Long id,
-        String loginId,
-        Role role,
         String token
 ) {
 }
