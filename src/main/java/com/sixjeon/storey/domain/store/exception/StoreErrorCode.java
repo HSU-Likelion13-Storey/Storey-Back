d26966d8e347fe6e3e7fee684b76c983890fc14d
zzuhannn <lunaaeric41@naver.com>
2025-08-10 00:53:07 +0900

    feat: 사업자 번호 불일치 예외 커스텀 추가

---
+++ src/main/java/com/sixjeon/storey/domain/store/exception/StoreErrorCode.java
@@ -0,0 +1,16 @@
+package com.sixjeon.storey.domain.store.exception;
+
+import com.sixjeon.storey.global.response.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StoreErrorCode implements BaseResponseCode {
+    STORE_INVALID_BUSINESS_NUMBER_400("STORE_INVALID_BUSINESS_NUMBER_400", 400, "사업자번호가 일치하지 않습니다."),
+    STORE_DUPLICATE_BUSINESS_NUMBER_409("STORE_DUPLICATE_BUSINESS_NUMBER_409", 409, "이미 등록된 사업자입니다.");
+
+    private final String code;
+    private final int httpStatus;
+    private final String message;
+}
