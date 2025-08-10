9f5f517720560598b78340bfb314ff04bfddd96d
zzuhannn <lunaaeric41@naver.com>
2025-08-10 19:46:26 +0900

    feat: 이미 등록된 가게 예외 커스텀 추가

--- src/main/java/com/sixjeon/storey/domain/store/exception/StoreErrorCode.java
+++ src/main/java/com/sixjeon/storey/domain/store/exception/StoreErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum StoreErrorCode implements BaseResponseCode {
     STORE_INVALID_BUSINESS_NUMBER_400("STORE_INVALID_BUSINESS_NUMBER_400", 400, "사업자번호가 일치하지 않습니다."),
-    STORE_DUPLICATE_BUSINESS_NUMBER_409("STORE_DUPLICATE_BUSINESS_NUMBER_409", 409, "이미 등록된 사업자입니다.");
+    STORE_DUPLICATE_BUSINESS_NUMBER_409("STORE_DUPLICATE_BUSINESS_NUMBER_409", 409, "이미 등록된 사업자입니다."),
+    STORE_ALREADY_REGISTERED_409("STORE_ALREADY_REGISTERED_409", 409, "이미 등록된 가게가 존재합니다.");
 
     private final String code;
     private final int httpStatus;
