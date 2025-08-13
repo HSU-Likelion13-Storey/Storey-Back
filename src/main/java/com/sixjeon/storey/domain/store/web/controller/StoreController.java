e06e9e2131eb689281747a00a61eb44f155652a8
zzuhannn <lunaaeric41@naver.com>
2025-08-14 03:15:23 +0900

    fix: EndPoint 변경

--- src/main/java/com/sixjeon/storey/domain/store/web/controller/StoreController.java
+++ src/main/java/com/sixjeon/storey/domain/store/web/controller/StoreController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/stores")
+@RequestMapping("/owner/store")
 @RequiredArgsConstructor
 public class StoreController {
 
