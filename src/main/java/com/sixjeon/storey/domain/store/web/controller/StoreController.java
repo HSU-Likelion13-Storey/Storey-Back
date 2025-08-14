ad985a59b6665acc999d2c34e9e4ac000f2f4cb3
zzuhannn <lunaaeric41@naver.com>
2025-08-15 00:11:20 +0900

    feat: 가게 전체 조회 API controller 작성

--- src/main/java/com/sixjeon/storey/domain/store/web/controller/StoreController.java
+++ src/main/java/com/sixjeon/storey/domain/store/web/controller/StoreController.java
@@ -1,6 +1,7 @@
 package com.sixjeon.storey.domain.store.web.controller;
 
 import com.sixjeon.storey.domain.store.service.StoreService;
+import com.sixjeon.storey.domain.store.web.dto.MapStoreRes;
 import com.sixjeon.storey.domain.store.web.dto.RegisterStoreReq;
 import com.sixjeon.storey.global.response.SuccessResponse;
 import com.sixjeon.storey.global.security.details.CustomUserDetails;
@@ -10,24 +11,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/owner/store")
+@RequestMapping
 @RequiredArgsConstructor
 public class StoreController {
 
     private final StoreService storeService;
 
-    @PostMapping
+    @PostMapping("/owner/store")
     public ResponseEntity<SuccessResponse<?>> registerStore(
             @Valid @RequestBody RegisterStoreReq registerStoreReq,
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         storeService.registerStore(registerStoreReq, customUserDetails.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(SuccessResponse.created("가게 정보 성공정으로 등록되었습니다."));
-            }
+    }
+
+    // 지도에서 모든 가게 조회
+    @GetMapping("/stores/map")
+    public ResponseEntity<SuccessResponse<?>> getStoresForMap() {
+        List<MapStoreRes> mapStoreRes = storeService.findAllStoresForMap();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.ok(mapStoreRes));
+    }
+
+
+
 }
