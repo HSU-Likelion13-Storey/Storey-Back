02ba033ac8e423aa20375f25f8803d2e8fdea6cc
zzuhannn <lunaaeric41@naver.com>
2025-08-18 03:32:51 +0900

    hotfix: CORS 활성화

--- src/main/java/com/sixjeon/storey/global/config/SecurityConfig.java
+++ src/main/java/com/sixjeon/storey/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.sixjeon.storey.global.security.jwt.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session ->  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .authorizeHttpRequests(auth -> auth
                                 .requestMatchers("/auth/signup", "/auth/owner/login", "/auth/user/login", "/auth/refresh").permitAll()
