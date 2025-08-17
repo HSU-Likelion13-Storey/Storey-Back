b7f39651b285c21ba5cae803f98ca1a824b54581
zzuhannn <lunaaeric41@naver.com>
2025-08-17 17:59:48 +0900

    delete: 사용하지 않는 예외 제거

--- src/main/java/com/sixjeon/storey/global/security/exception/RefreshTokenInvalidException.java
+++
@@ -1,9 +0,0 @@
-package com.sixjeon.storey.global.security.exception;
-
-import com.sixjeon.storey.global.exception.BaseException;
-
-public class RefreshTokenInvalidException extends BaseException {
-    public RefreshTokenInvalidException() {
-        super(RefreshTokenErrorCode.REFRESH_TOKEN_INVALID_400);
-    }
-}
