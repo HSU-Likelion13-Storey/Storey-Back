9ff8d1ae43d4c809cfbc381818120e4687ecf3f4
zzuhannn <lunaaeric41@naver.com>
2025-08-17 18:00:15 +0900

    delete: 사용하지 않는 예외 제거

--- src/main/java/com/sixjeon/storey/global/security/exception/RefreshTokenErrorCode.java
+++ src/main/java/com/sixjeon/storey/global/security/exception/RefreshTokenErrorCode.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RefreshTokenErrorCode implements BaseResponseCode {
     REFRESH_TOKEN_EXPIRED_401("REFRESH_TOKEN_EXPIRED_401",401,"리프레시 토큰이 만료되었습니다."),
-    REFRESH_TOKEN_INVALID_400("REFRESH_TOKEN_INVALID_400",400,"유효하지 않은 리프레시 토큰입니다."),
     REFRESH_TOKEN_NOT_FOUND_404("REFRESH_TOKEN_NOT_FOUND_404",404,"리프레시 토큰를 찾을 수 없습니다.");
 
 
