100b7bc8e80929dfd389836e0554193392dfaa2e
zzuhannn <lunaaeric41@naver.com>
2025-08-18 01:01:35 +0900

    chore: CORS 허용 도메인에 Vercel URL 추가

--- src/main/java/com/sixjeon/storey/global/config/WebConfig.java
+++ src/main/java/com/sixjeon/storey/global/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 실제 프론트 주소 명시
+                .allowedOrigins("https://storey-6jeon.vercel.app",
+                        "http://localhost:5173") // 실제 프론트 주소 명시
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
