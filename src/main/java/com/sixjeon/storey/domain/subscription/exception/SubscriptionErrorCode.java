6e9aed0441692a0d83293c756dd628464aaf4329
zzuhannn <lunaaeric41@naver.com>
2025-08-17 21:32:55 +0900

    feat: 무료 체험 불가 예외 커스텀 추가

--- src/main/java/com/sixjeon/storey/domain/subscription/exception/SubscriptionErrorCode.java
+++ src/main/java/com/sixjeon/storey/domain/subscription/exception/SubscriptionErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SubscriptionErrorCode implements BaseResponseCode {
-    SUBSCRIPTION_NOT_FOUND_404("SUBSCRIPTION_NOT_FOUND_404", 404, "구독 정보를 찾을 수 없습니다.");
+    SUBSCRIPTION_NOT_FOUND_404("SUBSCRIPTION_NOT_FOUND_404", 404, "구독 정보를 찾을 수 없습니다."),
+    SUBSCRIPTION_INVALID_STATUS_409("SUBSCRIPTION_INVALID_STATUS_409", 409, "무료 체험을 시작할 수 없는 상태입니다.");
 
 
     private final String code;
